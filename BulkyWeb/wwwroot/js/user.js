54c92370b45b3c2f49021018adea1f6749d2ceb6
Rubel <hossainrobiul33@gmail.com>
2024-11-29 03:04:32 +0600

    User Management

---
+++ BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,61 @@
+﻿var dataTable;
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/Admin/User/GetAll' },
+        "columns": [
+            { "data": "name", "width":"15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data": { "id":"id", "lockoutEnd": "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        return `
+                        <div class="text-center">
+                            <a onClick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-lock-fill"></i>Lock
+                            </a>
+                            <a href="/Admin/User/RoleManagment?userId=${data.id}" class="btn btn-primary text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i>Permission
+                            </a>
+                        </div>
+                    `;
+                    } else {
+                        return `
+                        <div class="text-center">
+                            <a onClick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer; width:100px;">
+                                <i class="bi bi-unlock-fill"></i>Unlock
+                            </a>
+                            <a href="/Admin/User/RoleManagment?userId=${data.id}" class="btn btn-primary text-white" style="cursor:pointer; width:150px;">
+                                <i class="bi bi-pencil-square"></i>Permission
+                            </a>
+                        </div>
+                    `;
+                    }
+
+                },
+                "width": "25%"
+            }
+        ]
+    });
+}
+
+function LockUnlock(id) {
+    $.ajax({
+        url: '/Admin/User/LockUnlock',
+        type: 'POST',
+        data: JSON.stringify(id),
+        contentType:"application/json",
+        success: function (data) {
+            dataTable.ajax.reload();
+            toastr.success(data.message);
+        }
+    });
+}
